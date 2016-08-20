07c25cfeb3136a6bc90883004b5c83f9b04deb10
Thieu Mao <nguyen.van.thieub@framgia.com>
2016-08-20 14:11:34 +0700

    1 - Init Project

---
+++ TNUSocial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TNUSocial
+//
+//  Created by Thieu Mao on 8/20/16.
+//  Copyright © 2016 OTVINA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
