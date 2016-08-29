f7a8109e76ae2afc31f560748ec9c5f330a326dc
framgia <framgia@framgias-Mac-mini-6.local>
2016-08-30 02:07:24 +0700

    25 - Add Const

--- TNUSocial/Utils/MyConst.h
+++ TNUSocial/Utils/MyConst.h
@@ -12,6 +12,12 @@
 
 #pragma mark - Link API
 extern NSString *const URL_LOGIN;
+extern NSString *const URL_THONG_TIN_SINH_VIEN;
+extern NSString *const URL_LICH_HOC;
+extern NSString *const URL_THONG_BAO_NHA_TRUONG;
+extern NSString *const URL_CHI_TIET_THONG_BAO;
+extern NSString *const URL_DIEM_SINH_VIEN;
+extern NSString *const URL_LICH_THI;
 
 #pragma mark - Param API
 extern NSString *const FROM;
@@ -19,5 +25,15 @@ extern NSString *const APP_ID;
 extern NSString *const APP_SECRET;
 extern NSString *const USERNAME;
 extern NSString *const PASSWORD;
+extern NSString *const ACCESS_TOKEN;
+extern NSString *const NEWS_ID;
+extern NSString *const SEMESTER;
+extern NSString *const EXAMINATION;
+extern NSString *const SESSION;
+
+#pragma mark - Value Define
+extern NSString *const APP_ID_VALUE;
+extern NSString *const APP_SECRET_VALUE;
+extern NSString *const DOMAIN_VALLUE;
 
 @end
--- TNUSocial/Utils/MyConst.m
+++ TNUSocial/Utils/MyConst.m
@@ -11,7 +11,13 @@
 @implementation MyConst
 
 #pragma mark - Link API
-NSString *const URL_LOGIN = @"http://ictu.kingdark.org/api.php?api=login-app";
+NSString *const URL_LOGIN = @"api=login-app";
+NSString *const URL_THONG_TIN_SINH_VIEN = @"api=get&path=user";
+NSString *const URL_LICH_HOC = @"api=get&path=student-time-table";
+NSString *const URL_THONG_BAO_NHA_TRUONG = @"api=get&path=news";
+NSString *const URL_CHI_TIET_THONG_BAO = @"api=get&path=news-detail";
+NSString *const URL_DIEM_SINH_VIEN = @"api=get&path=student-mark";
+NSString *const URL_LICH_THI = @"api=get&path=student-exam-list";
 
 #pragma mark - Param API
 NSString *const FROM = @"from";
@@ -19,5 +25,15 @@ NSString *const APP_ID = @"app-id";
 NSString *const APP_SECRET = @"app-secret";
 NSString *const USERNAME = @"username";
 NSString *const PASSWORD = @"password";
+NSString *const ACCESS_TOKEN = @"access-token";
+NSString *const NEWS_ID = @"newsid";
+NSString *const SEMESTER = @"semester";
+NSString *const EXAMINATION = @"examination";
+NSString *const SESSION = @"session";
+
+#pragma mark - Value Define
+NSString *const APP_ID_VALUE = @"475681656679";
+NSString *const APP_SECRET_VALUE = @"LsO189xl1p5b5673t87pQ05w6d3k9KeP";
+NSString *const DOMAIN_VALLUE = @"http://ictu.kingdark.org/api.php";
 
 @end
